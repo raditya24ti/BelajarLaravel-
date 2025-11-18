a67db6b34fabc862605b2560f1012e1fc9f4ce45
raditya24ti <raditya24ti@mahasiswa.pcr.ac.id>
2025-11-19 05:52:47 +0700

    g

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,17 +9,24 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-    
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
