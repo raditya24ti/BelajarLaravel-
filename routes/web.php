98a583daa2d6f6c0eda5220e4242059d68ff180b
raditya24ti <raditya24ti@mahasiswa.pcr.ac.id>
2025-12-02 12:24:31 +0700

    p12part4

--- routes/web.php
+++ routes/web.php
@@ -13,92 +13,100 @@ use App\Http\Controllers\MultipleuploadsController;
 
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
-
 });
 
 Route::get('/mahasiswa', function () {
-    return 'Hallo Mahasiswa';
-
+    return 'Halo Mahasiswa';
 });
 
-Route::get('/nama/{Zahwa}', function ($Zahwa) {
-    return 'Nama Saya: ' . $Zahwa;
-
+Route::get('/nama/{Yolanda}', function ($param1) {
+    return 'Nama Saya: '.$param1;
 });
 
-Route::get('/nim/{Zahwa}', function ($Zahwa = '') {
-    return 'Nim Saya: ' . $Zahwa;
-
+Route::get('/nim/{pram1?}', function ($param1 = '') {
+    return 'NIM Saya: '.$param1;
 });
-Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/mahasiswa/{Yolanda}', [MahasiswaController::class, 'show']);
 
 Route::get('/about', function () {
     return view('halaman-about');
-
 });
+
+Route::get('/matakuliahshow', [MatakuliahController::class, 'show']);
 Route::get('/matakuliahindex', [MatakuliahController::class, 'index']);
 Route::get('/matakuliahcreate', [MatakuliahController::class, 'create']);
-Route::get('/matakuliahstore', [MatakuliahController::class, 'store']);
-Route::get('/matakuliahshow', [MatakuliahController::class, 'show']);
+Route::get('/mataKuliahstore', [MatakuliahController::class, 'store']);
 Route::get('/matakuliahedit', [MatakuliahController::class, 'edit']);
 Route::get('/matakuliahupdate', [MatakuliahController::class, 'update']);
 Route::get('/matakuliahdestroy', [MatakuliahController::class, 'destroy']);
 
-Route::get('/matakuliah/show/{kode?}', [MatakuliahController::class, 'show']);
-
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/matakuliah/{param1?}', [MahasiswaController::class, 'show']);
+Route::get('/home', [HomeController::class, 'index'])-> name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
-    ->name('question.store');
+            -> name('question.store');
 
-Route::get('/home', function () {
+Route::get('dashboard', [DashboardController::class, 'index'])
+            -> name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
+Route::get('/pelanggan/{id}/edit', [PelangganController::class, 'edit'])->name('pelanggan.edit');
+Route::put('/pelanggan/{id}', [PelangganController::class, 'update'])->name('pelanggan.update');
+
+Route::resource('user', UserController::class);
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+
+
+Route::get('/auth', function () {
     return view('home');
 
 });
-// Route::get('/auth', function () {
-//     return view('home');
+Route::get('/auth', [AuthController::class, 'index']);
+Route::post('/auth/login', [AuthController::class, 'login']);
 
-// });
-// Route::get('/auth', [AuthController::class, 'index']);
-Route::get('/auth', [AuthController::class, 'index'])->name('login');
-Route::post('/auth/login', [AuthController::class,'login'])->name('auth.login');
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 
-// Route::get('/auth', [AuthController::class, 'index']);
-// Route::post('/auth/login', [AuthController::class, 'login']);
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
-Route::get('/pegawai', [PegawaiController::class, 'index']);
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('pelanggan', PelangganController::class);
-
-// Route::get('/admin', function () {
-//     return view('admin.template');
-// });
-
-Route::get('/admin', [DashboardController::class, 'index'])->name('admin');
-
-Route::resource('user', UserController::class);
-// Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
-// Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-// Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
-// Route::get('/user', [UserController::class, 'index'])->name('user.index');
-// Update profile (upload foto)
-Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-
-// Delete foto
-Route::delete('/profile/delete', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-// Display profile
-// Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-
-
-Route::get('/profile', [ProfileController::class, 'index']);
-
-
-// Edit profile
-Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-
+// Route::get('/multipleuploads', 'MultipleuploadsController@index')->name('uploads');
+// Route::post('/save','MultipleuploadsController@store')->name('uploads.store');
 
 Route::get('/multipleuploads', [MultipleuploadsController::class, 'index'])->name('uploads');
 Route::post('/save', [MultipleuploadsController::class, 'store'])->name('uploads.store');
+
+// halaman guest
+Route::middleware('guest')->group(function () {
+    // Halaman Form Login
+    Route::get('/auth', [AuthController::class, 'index'])->name('login');
+
+    // Proses Submit Login
+    Route::post('/auth/login', [AuthController::class, 'login'])->name('login.process');
+});
+
+// Halaman Depan
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// halaman wajib login
+Route::middleware('auth')->group(function () {
+
+    // Logout (Bisa diakses semua user yang login)
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// --- DASHBOARD UNTUK USER BIASA ---
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Fitur User Biasa (Contoh: Kirim Pertanyaan)
+    Route::post('question/store', [QuestionController::class, 'store'])->name('question.store');
+    Route::get('/home', [HomeController::class, 'index']);
+
+    // Khusus admin
+    Route::middleware(['role:admin'])->prefix('admin')->group(function () {
+        Route::resource('user', UserController::class);
+        Route::resource('pelanggan', PelangganController::class);
+    });
+});
